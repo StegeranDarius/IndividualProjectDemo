0108015b0012c6e90e9e53b9271ee1a1a5342aaf
StegeranDarius <116189987+StegeranDarius@users.noreply.github.com>
2022-12-04 19:11:07 +0200

    Add files via upload

---
+++ To_Do/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.example.GUI.MainWindow;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        MainWindow window = new MainWindow();
+        window.setLocationRelativeTo( null );
+        window.setVisible(true);
+    }
+
+
+}
